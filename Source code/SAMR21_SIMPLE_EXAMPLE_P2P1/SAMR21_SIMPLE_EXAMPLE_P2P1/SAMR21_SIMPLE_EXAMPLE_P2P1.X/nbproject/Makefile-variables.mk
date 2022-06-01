d05fbcc194728b052a3eeffac0113dc443444479
Jerry Chen - A23125 <jerry.qx.chen@microchip.com>
2022-06-01 10:03:02 +0800

    first commit

---
+++ Source code/SAMR21_SIMPLE_EXAMPLE_P2P1/SAMR21_SIMPLE_EXAMPLE_P2P1/SAMR21_SIMPLE_EXAMPLE_P2P1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Release configuration
+CND_ARTIFACT_DIR_Release=dist/Release/production
+CND_ARTIFACT_NAME_Release=SAMR21_SIMPLE_EXAMPLE_P2P1.X.production.hex
+CND_ARTIFACT_PATH_Release=dist/Release/production/SAMR21_SIMPLE_EXAMPLE_P2P1.X.production.hex
+CND_PACKAGE_DIR_Release=${CND_DISTDIR}/Release/package
+CND_PACKAGE_NAME_Release=samr21simpleexamplep2p1.x.tar
+CND_PACKAGE_PATH_Release=${CND_DISTDIR}/Release/package/samr21simpleexamplep2p1.x.tar
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=SAMR21_SIMPLE_EXAMPLE_P2P1.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/SAMR21_SIMPLE_EXAMPLE_P2P1.X.production.hex
+CND_PACKAGE_DIR_Debug=${CND_DISTDIR}/Debug/package
+CND_PACKAGE_NAME_Debug=samr21simpleexamplep2p1.x.tar
+CND_PACKAGE_PATH_Debug=${CND_DISTDIR}/Debug/package/samr21simpleexamplep2p1.x.tar
